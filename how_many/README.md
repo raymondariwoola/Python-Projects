db53d5033c3213d7e96acefe2988c8fde11ec3a0
Raymond Ariwoola <Raymondariwoola@gmail.com>
2022-06-12 16:12:02 +0400

    add more conversion methods

---
+++ how_many/README.md
@@ -0,0 +1,30 @@
+# Various conversions
+
+### convert Year to any
+
+1. Years to Months
+2. Years to Days
+3. Years to hours
+4. Years to minutes
+5. Years to seconds
+
+## Usage
+
+```python
+convertDate = convert_YDHM(1)
+
+# Years to Months
+convertDate.convert_years_to_months()
+
+# Years to Days
+convertDate.convert_years_to_days()
+
+# Years to hours
+convertDate.convert_years_to_hours()
+
+# Years to minutes
+convertDate.convert_years_to_minutes()
+
+# Years to seconds
+convertDate.convert_years_to_seconds()
+```
--- how_many/app.py
+++ how_many/app.py
@@ -8,33 +8,42 @@ class convert_YDHM:
     def __str__(self):
         outcome = "You have entered {} year".format(self.years)
         return outcome
-    
-    def convert_years_to_days(self):
-        years = int(self.years)
-        days = years * 365
-        outcome = "{} years is {} days".format(years, hours)
-        return outcome
-    
 
-    def convert_years_to_hours(self):
+    def calculate(self):
         years = int(self.years)
-        days = years * 365
-        hours = days * 24
-        outcome = "{} years is {} hours".format(years, hours)
-        return outcome
-
-
-    def convert_years_to_Minutes(self):
-        years = int(self.years)
+        months = years * 12
         days = years * 365
         hours = days * 24
         minutes = hours * 60
-        outcome = "{} years is {} minutes".format(years, minutes)
+        seconds = minutes * 60
+        outcome = {"years": years, "months": months, "days": days,
+                   "hours": hours, "minutes": minutes, "seconds": seconds}
+        return outcome
+
+    def convert_years_to_months(self):
+        months = self.calculate()["months"]
+        outcome = "{} years is {} months".format(self.years, months)
+        return outcome
+
+    def convert_years_to_days(self):
+        days = self.calculate()["days"]
+        outcome = "{} years is {} days".format(self.years, days)
+        return outcome
+
+    def convert_years_to_hours(self):
+        hours = self.calculate()["hours"]
+        outcome = "{} years is {} hours".format(self.years, hours)
+        return outcome
+
+    def convert_years_to_minutes(self):
+        minutes = self.calculate()["minutes"]
+        outcome = "{} years is {} minutes".format(self.years, minutes)
+        return outcome
+
+    def convert_years_to_seconds(self):
+        seconds = self.calculate()["seconds"]
+        outcome = "{} years is {} seconds".format(self.years, seconds)
         return outcome
 
 
-convert = convert_YDHM(2)
-convert.convert_years_to_hours()
 
-# TODO
-# ! refactor class
